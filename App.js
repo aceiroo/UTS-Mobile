cce6288d9e9ca0eb8ebc2d90537f169ac8c43b05
audie <audiedevanugraha@gmail.com>
2020-11-27 21:28:18 +0800

    third

--- App.js
+++ App.js
@@ -1,21 +1,125 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer, getFocusedRouteNameFromRoute, DrawerActions,} from "@react-navigation/native";
+import {createStackNavigator} from "@react-navigation/stack";
+import { StyleSheet, Text, View, Button } from 'react-native';
+import SignInScreen from './src/SignIn'
+import SignUpScreen from './src/SignUp'
+import PasswordForgetScreen from './src/PasswordForget'
+import PasswordChangeScreen from './src/PasswordChange'
+import HomeScreen from './src/Home'
+import ProfileScreen from './src/Profile';
+import AccountScreen from './src/Account'
+import { createDrawerNavigator,DrawerContentScrollView,DrawerItemList,DrawerItem, } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {MaterialCommunityIcons} from '@expo/vector-icons'
 
-export default function App() {
+const Drawer = createDrawerNavigator();
+ 
+const Tab = createBottomTabNavigator();
+ 
+const HomeTabs = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen}  options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+          ),
+        }}/>
+      <Tab.Screen name="Profile" component={ProfileScreen} options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="account" color={color} size={26} />
+          ),
+        }}/>
+    </Tab.Navigator>
+  );
+};
+ 
+const CustomDrawerContent = (props) => {
+  return (
+    <DrawerContentScrollView {...props}>
+      <DrawerItemList {...props} />
+      <DrawerItem label="Log Out" onPress={props.onSignOut} />
+    </DrawerContentScrollView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const HomeDrawer = ({onSignOut}) => {
+  return (
+    <Drawer.Navigator  drawerContent={props => <CustomDrawerContent {...props} onSignOut={onSignOut} />} >
+      <Drawer.Screen name="Home" component={HomeTabs} />
+      <Drawer.Screen name="Account" component={AccountScreen} />
+      <Drawer.Screen
+        name="Change Password"
+        component={PasswordChangeScreen}
+      />
+      
+      
+    </Drawer.Navigator>
+  );
+};
+
+const RootStack = createStackNavigator();
+ 
+const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = React.useState(false);
+  const Hshown = 
+  isAuthenticated ? ( false ) : ( true );
+
+  const handleSignIn = () => {
+ 
+    setIsAuthenticated(true);
+  };
+
+  const handleSignOut = () => {
+ 
+    setIsAuthenticated(false);
+  };
+
+  const handleSignUp = () => {
+ 
+    setIsAuthenticated(true);
+  };
+
+  return (
+    <NavigationContainer>
+    <RootStack.Navigator
+      screenOptions={{
+        headerShown: Hshown
+      }}
+    >
+      {isAuthenticated ? (
+       <RootStack.Screen name="Home">
+       {(props) => (
+         <HomeDrawer {...props} onSignOut={handleSignOut} />
+       )}
+     </RootStack.Screen>
+      ) : (
+        <>
+          <RootStack.Screen name="Sign In">
+            {(props) => (
+              <SignInScreen {...props} onSignIn={handleSignIn} />
+            )}
+          </RootStack.Screen>
+          <RootStack.Screen name="Sign Up">
+            {(props) => (
+              <SignUpScreen {...props} onSignUp={handleSignUp} />
+            )}
+          </RootStack.Screen>
+          <RootStack.Screen name="Password Forget">
+              
+            {(props) => (
+              <PasswordForgetScreen {...props} onSignIn={handleSignIn}/>
+            )}
+          </RootStack.Screen>
+        </>
+      )}
+    </RootStack.Navigator>
+  </NavigationContainer>
+  );
+};
+ 
+export default App;
+   
